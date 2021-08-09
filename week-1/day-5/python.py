e481dd16217333d576a7696fee47fe4665e2adfe
jjagui83 <joseaguilar0583@yahoo.com>
2021-08-09 14:55:32 -0500

    dantesInferno
    navigate through the levels

--- week-1/day-5/python.py
+++ week-1/day-5/python.py
@@ -1,10 +1,21 @@
-ogAvengers = ["Captain Americs", "Thor", "Iron Man", "Black Widow", "Hulk"]
+# ogAvengers = ["Captain Americs", "Thor", "Iron Man", "Black Widow", "Hulk"]
 
 
-ogAvengers.append(["Vison", "Hawkeye", "Falcon", "Scarlet Witch", "War Machine"])
+# ogAvengers.append(["Vison", "Hawkeye", "Falcon", "Scarlet Witch", "War Machine"])
 
-del ogAvengers[2]
+# del ogAvengers[2]
 
-ogAvengers.insert(3, "spiderman")
+# ogAvengers.insert(3, "spiderman")
 
-print(ogAvengers)
+# print(ogAvengers)
+
+
+# print("hello")
+
+def divide(dividend, divisor):
+    quotient = dividend // divisor
+    remainder = dividend % divisor
+    return (quotient, remainder)
+
+result = divide(5,3)
+print(result)
---
+++ week-1/day-6/mapping.py
@@ -0,0 +1,30 @@
+# friend info = {
+#     "Name" : "Alan Turing",
+#     "Cell" : 
+# }
+
+ramit = {
+    "name" : "Rammit",
+    "email" : "ramit@global.com",
+    "interests" : ["movies", "tennis"],
+    "friends" : [
+        {
+            "name" : "Jasmine",
+            "email" : "jasmine@yahoo.com",
+            "interest" : ["photography" , 'tennis']
+
+        },
+        {
+            'name' : 'Jan',
+            'email' : 'jan@hotmail.com',
+            'interests' : ["movies", "tv"]
+        }
+    ],
+    "friends_count" : 2 
+}
+
+# print(ramit["email"])
+# print(ramit["interests"][0])
+# print(ramit["friends"][0]["email"])
+# print(ramit["friends"][1]["interests"][1])
+
