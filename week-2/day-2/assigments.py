7b3ec5757bbdfeb11232a358d89bb785c2e0a1c3
jjagui83 <joseaguilar0583@yahoo.com>
2021-08-12 09:29:58 -0500

    Revert "Update assigments.py"
    This reverts commit e3c4dda10c96bd5c3f61e9b381b377d6d03476a4.

--- week-2/day-2/assigments.py
+++ week-2/day-2/assigments.py
@@ -31,17 +31,69 @@
 # print(david.add_address)
 
 
-class Table:
-    def __init__(self):
-        self.height =
-        self.material =
-        self.seats =
-        self.color =
+# class Table:
+#     def __init__(self):
+#         self.height =
+#         self.material =
+#         self.seats =
+#         self.color =
 
 
-class Rectangular(Table):
+# class Rectangular(Table):
+
+myDict = []
+class Grocery_List:
+    def __init__(self, store):
+        self.store = store
+    def __eq__(self, another):
+        return hasattr(another, 'store') and self.store == another.store
+    def __hash__(self):
+        return hash(self.store)
+    def __repr__(self):
+        return ("%s" % self.store)
+def welcomeMessage():
+    message = """
+    Welcome to your shopping list directory.
+    1. Add a list
+    2. Add an item to a list
+    3. View a list
+    4. Press q to quit
+    """
+    return print(message)
+
+def makingAList():
+    listToAdd = []
+    userInput = input("What do you want to call your new list? ")
+    listInput = Grocery_List(userInput)
+    listToAdd.append(listInput)
+    addAnItem = input("What item do you want to add? ")
+    listToAdd.append(addAnItem)
+    myDict.append(listToAdd)
+
+def addItemtoList():
+    for store in myDict:
+        print(store)
+    userInput = int(input("What list do you want to add to? "))
+    itemToAdd = input("What do you want to add? ")
+    myDict[userInput].append(itemToAdd)
 
 
+choice = ""
+while(choice != "q"):
+    welcomeMessage()
+    userChoice = input("What do you want to do? ")
+    if userChoice == "1":
+        makingAList()
+    if userChoice == "2":
+        addItemtoList()
+    if userChoice == "3":
+        for store in myDict:
+            print(store)
+    if userChoice == "q":
+        print("Thanks for accessing your lists.")
+        break
+
+print(myDict)
 
 
 
