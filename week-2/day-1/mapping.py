738435bfab43cc6ce7cdfe804d04f3cad38c99c9
jjagui83 <joseaguilar0583@yahoo.com>
2021-08-12 09:40:39 -0500

    Update mapping.py

--- week-2/day-1/mapping.py
+++ week-2/day-1/mapping.py
@@ -27,24 +27,3 @@
 # # print(ramit["interests"][0])
 # # print(ramit["friends"][0]["email"])
 # # print(ramit["friends"][1]["interests"][1])
-
-superhero = {
-    "name": "Wonder Woman",
-    "alias": "Diana Prince",
-    "gear": [
-        "Lasso of Truth",
-        "Bracelets of Submission"
-    ],
-    "vehicle": {
-        "title": "Invisible Jet",
-        "speed": "2000 mph",
-    }
-}
-
-for item in superhero["gear"]:
-    print("%s has %s" % (superhero["name"], item))
-
-file_name = "mapping.py"
-with open(file_name, "r") as file_handle:
-    contents = file_handle.read()
-    print(contents)
