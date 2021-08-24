e83539d2d1870fde497d34a3d84299ec0fb7535a
jjagui83 <joseaguilar0583@yahoo.com>
2021-08-23 20:51:57 -0500

    exercises
    finally got firs three

---
+++ week-4/script.js
@@ -0,0 +1,4 @@
+function madLib(name,subject) {
+    return `${name} enjoys ${subject}`;
+}
+console.log(madLib('Jose',"math"));
---
+++ week-4/tip.js
@@ -0,0 +1,19 @@
+// const good = 20
+// const fair = 15
+// const bad = 10
+// function tipAmount (bill, service) {
+//     let level = service / 100;
+//     let tip = bill * level
+//     console.log(tip)
+// }
+// tipAmount(100,bad)
+
+// const good = 20
+// const fair = 15
+// const bad = 10
+// function tipAmount (bill, service) {
+//     let level = service / 100;
+//     let tip = bill * level
+//     console.log(bill + tip)
+// }
+// tipAmount(100,good)
